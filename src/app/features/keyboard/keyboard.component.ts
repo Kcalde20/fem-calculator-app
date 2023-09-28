389a080e094219cd8ad1ddc460532434591eab66
Kevin Calderwood <kevincalderwood001@comcast.net>
2023-09-27 20:05:15 -0400

    keyboard component

---
+++ src/app/features/keyboard/keyboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './keyboard.component.html',
+  styleUrls: ['./keyboard.component.scss'],
+  selector: 'app-keyboard-component'
+})
+export class KeyboardComponent {
+
+}
