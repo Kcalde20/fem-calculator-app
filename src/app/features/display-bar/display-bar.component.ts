34f348233fdab7aad45c6d2b8748662f30f8a64f
Kevin Calderwood <kevincalderwood001@comcast.net>
2023-09-27 20:05:22 -0400

    display bar component

---
+++ src/app/features/display-bar/display-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-bar',
+  templateUrl: './display-bar.component.html',
+  styleUrls: ['./display-bar.component.scss']
+})
+export class DisplayBarComponent {
+
+}
